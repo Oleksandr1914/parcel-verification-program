8b1c770bb636b6993aaadfab8c238d5a14f3ecd4
Oleksandr_Mochakov <sasha.mochakov@ukr.net>
2023-08-10 14:30:48 +0300

    added tablet and desctop styles to delivery component

--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -24,14 +24,13 @@ export const BlockVerification = styled.div`
   flex-direction: column;
   gap: 40px;
   width: 100%;
-  min-height: 480px;
-  padding: 15px;
-  padding-top: 30px;
+  min-height: 380px;
+  padding: 20px 15px;
   background-color: rgb(251 251 251);
   border-radius: 8px;
   box-shadow: 2px 4px 9px 0px rgba(166, 141, 174, 0.28);
   ${device.desktop} {
     width: 980px;
-    padding: 15px 60px;
+    padding: 30px 60px;
   }
 `;
